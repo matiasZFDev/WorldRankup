e74ed45f9208135b5d2c6899535c24019ddf3ff6
matiasZFDev <cbudd333@gmail.com>
2023-04-04 13:51:01 -0400

    missing var renaming

--- src/main/java/com/worldplugins/rankup/NBTKeys.java
+++ src/main/java/com/worldplugins/rankup/NBTKeys.java
@@ -3,8 +3,7 @@ package com.worldplugins.rankup;
 import lombok.NonNull;
 
 public class NBTKeys {
-    public static final @NonNull String BAG_SHARD_ID = "wrankup_bag_shard_id";
-    public static final @NonNull String BAG_SHARD_AMOUNT = "wrankup_bag_shard_amount";
+    public static final @NonNull String BAG_SHARD = "wrankup_bag_shard_id";
     public static final @NonNull String PHYISIC_SHARD_ID = "wrankup_physic_shard_id";
     public static final @NonNull String PHYISIC_SHARD_AMOUNT = "wrankup_physic_shard_amount";
     public static final @NonNull String PHYISIC_LIMIT_ID = "wrankup_physic_limit_id";
