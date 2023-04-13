11652d6693d61cd181c2db9e72fe84b589de8c90
matiasZFDev <cbudd333@gmail.com>
2023-04-13 19:16:34 -0400

    evolution manager current access fix

--- src/main/java/com/worldplugins/rankup/manager/EvolutionManager.java
+++ src/main/java/com/worldplugins/rankup/manager/EvolutionManager.java
@@ -18,7 +18,7 @@ public class EvolutionManager {
     public void setRank(@NonNull Player player, short rank) {
         playerService.consumePlayer(player.getUniqueId(), playerModel -> {
             final RanksConfig.Config.Rank playerRank = ranksConfig.get().getById(playerModel.getRank());
-            final RanksConfig.Config.Rank newRank = ranksConfig.get().getPrevious(rank);
+            final RanksConfig.Config.Rank newRank = ranksConfig.get().getById(rank);
 
             playerModel.setRank(rank);
             permissionManager.removeGroup(player, playerRank.getGroup());
@@ -29,7 +29,7 @@ public class EvolutionManager {
     public void setPrestige(@NonNull Player player, short prestige) {
         playerService.consumePlayer(player.getUniqueId(), playerModel -> {
             final Prestige playerPrestige = prestigeConfig.get().getPrestiges().getById(playerModel.getPrestige());
-            final Prestige newPrestige = prestigeConfig.get().getPrestiges().getPrevious(prestige);
+            final Prestige newPrestige = prestigeConfig.get().getPrestiges().getById(prestige);
 
             playerModel.setPrestige(prestige);
             permissionManager.removeGroup(player, playerPrestige.getGroup());
