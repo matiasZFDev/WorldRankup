4b4bab521e13c3dd654a6a23f3e384731162238e
matiasZFDev <cbudd333@gmail.com>
2023-06-01 05:01:57 -0400

    local worldlib push

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ val projectFullName = "${project.name}-LATEST.jar";
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
-    compileOnly(files("/home/post/dev/bukkit-libs/WorldLib-LATEST.jar"))
+    compileOnly("com.worldplugins:WorldLib:LATEST")
     compileOnly(files("/home/post/dev/bukkit-libs/LegendChat.jar"))
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude(group = "org.bukkit", module = "bukkit")
